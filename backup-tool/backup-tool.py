651afc3c5cba47b8304408c327b644f4e77f58ee
karol.siedlaczek <karol.siedlaczek@redge.com>
2022-03-23 23:40:37 +0100

    2022-03-23

--- backup-tool/backup-tool.py
+++ backup-tool/backup-tool.py
@@ -10,8 +10,8 @@ import argparse
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-LOG_FILE = f'/var/log/{os.path.basename(__file__).split(".")[0]}.log'
-# LOG_FILE = f'{os.path.abspath(os.path.dirname(__file__))}/logs/{os.path.basename(__file__).split(".")[0]}.log'
+# LOG_FILE = f'/var/log/{os.path.basename(__file__).split(".")[0]}.log'
+LOG_FILE = f'{os.path.abspath(os.path.dirname(__file__))}/logs/{os.path.basename(__file__).split(".")[0]}.log'
 PASSWORD_FILE = '/root/.ssh/.password'
 MOUNT_POINT = '/storage/raid1'
 STORAGE_USER = 'storage'
@@ -21,6 +21,7 @@ logging.basicConfig(filename=LOG_FILE, format='%(asctime)s | %(name)s | %(leveln
 
 def make_backup(directories, user, max_num_of_backups):
     regex_pattern = r'^(\d{3}.\d{3}.\d{1}.\d{2,3})'
+    curr_timestamp = datetime.now().strftime('%Y%m%d%H%M.%S')
     ssh_conn = False
     dest_dir = directories[-1]
     check_if_md0_mounted()
@@ -48,8 +49,7 @@ def make_backup(directories, user, max_num_of_backups):
     send_rsync(directories, user, ssh_conn)
     logging.debug(f'backup complete "{dest_dir}/{backup_filename}"')
     os.system(f'chown -R {STORAGE_USER} {dest_dir}/{backup_filename}')
-    curr_timestamp = datetime.now().strftime('%Y%m%d%H%M.%S')
-    os.system(f'touch -t {curr_timestamp} {directories[1]}')
+    os.system(f'touch -t {curr_timestamp} {dest_dir}/{backup_filename}')  # change mtime on backup creation date
 
 
 def send_rsync(dirs, user, ssh_conn):
