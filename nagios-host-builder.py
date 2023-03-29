57851470f1a6e5b495731b1adc74e31c5e114b53
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2023-03-29 22:02:14 +0200

    Create nagios-host-builder.py

---
+++ nagios-host-builder.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    filename = 'hosts.txt'
+    host_group_name = 'waw5-fo-hosts'
+    members = []
+    with open('output.txt', 'w') as f_w:
+        f_w.write('')
+    with open(filename, 'r') as f_r:
+        for line in f_r.readlines():
+            line_split = line.replace('\n', '').split(':')
+            host_name = line_split[0]
+            host_address = line_split[1]
+            with open('output.txt', 'a') as f_w:
+                f_w.write('define host {\n')
+                f_w.write(f'\thost_name\t{host_name.lower()}\n')
+                f_w.write(f'\taddress\t\t{host_address}\n')
+                f_w.write(f'\tuse\t\tgeneric-host\n')
+                f_w.write('}\n\n')
+            members.append(host_name.lower())
+    print(','.join(members))
+    with open('output.txt', 'a') as f_w:
+        f_w.write('define hostgroup {\n')
+        f_w.write(f'\thostgroup_name\t{host_group_name}\n')
+        f_w.write(f'\tmembers\t\t{",".join(members)}\n')
+        f_w.write('}')
